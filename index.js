edd8b5d0758348204b30e01292b882d21240f67a
himanshu-sheetlani <himanshu.sheetlani2909@gmail.com>
2025-07-21 19:20:21 +0530

    routes completed

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-module.exports = {
-  router: require('./lib/router/index.mjs')
-  // Later: db: require('./lib/db'), auth: require('./lib/auth') etc.
+import './test.js'; // or './showcase.js'
+import app from './lib/router/_app.js';
+import get from './lib/router/get.js';
+import post from './lib/router/post.js';
+import patch from './lib/router/patch.js';
+import del from './lib/router/delete.js';
+
+export default {
+  get,
+  post,
+  patch,
+  del,
 };
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Jetend server running at http://localhost:${PORT}`);
+});
