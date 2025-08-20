85246ceff2af40fd4791ff8b8d43bc5a8712824b
himanshu-sheetlani <himanshu.sheetlani2909@gmail.com>
2025-08-20 19:15:11 +0530

    database added

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ import get from './lib/router/get.js';
 import post from './lib/router/post.js';
 import patch from './lib/router/patch.js';
 import del from './lib/router/delete.js';
+import db from './lib/db/_app.js';
 
 export default {
   get,
   post,
   patch,
   del,
+  db,
 };
 
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`🚀 Jetend server running at http://localhost:${PORT}`);
 });
