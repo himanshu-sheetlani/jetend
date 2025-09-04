44a7e283759ad12194125e58fc4f3dd9827b8a07
himanshu-sheetlani <himanshu.sheetlani2909@gmail.com>
2025-09-04 20:09:57 +0530

    improved exports method

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // jetend/index.js  (main entry for the package)
-
-// Router
 // index.js (root of JetEnd)
+
 import db from "./lib/db/_app.js";
 import * as auth from "./lib/auth/_app.js";
 import * as router from "./lib/router/_app.js";
 import * as utils from "./lib/utils/_app.js";
 
+export const { hashPassword, comparePassword, generateJWT, verifyJWT, requireAuth} = auth;
 export const { get, post, patch, del } = router;
+export const { validate, success, error, sendEmail} = utils;
+
 export { db, auth, router, utils };
